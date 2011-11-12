5dfd06a37571e2882fa6ab108f23987309989fd7
Ramiro Polla <ramiro.polla@gmail.com>
2011-11-12 18:41:59 -0200

    support only IE>=7, in which case XMLHttpRequest is always present

--- matrufsc.js
+++ matrufsc.js
@@ -1,23 +1,3 @@
-/*
-   Provide the XMLHttpRequest constructor for Internet Explorer 5.x-6.x:
-   Other browsers (including Internet Explorer 7.x-9.x) do not redefine
-   XMLHttpRequest if it already exists.
- 
-   This example is based on findings at:
-   http://blogs.msdn.com/xmlteam/archive/2006/10/23/using-the-right-version-of-msxml-in-internet-explorer.aspx
-*/
-if (typeof XMLHttpRequest == "undefined")
-  XMLHttpRequest = function () {
-    try { return new ActiveXObject("Msxml2.XMLHTTP.6.0"); }
-      catch (e) {}
-    try { return new ActiveXObject("Msxml2.XMLHTTP.3.0"); }
-      catch (e) {}
-    try { return new ActiveXObject("Microsoft.XMLHTTP"); }
-      catch (e) {}
-    //Microsoft.XMLHTTP points to Msxml2.XMLHTTP and is redundant
-    throw new Error("This browser does not support XMLHttpRequest.");
-  };
-
 function Combobox(input, suggestions, logger, lista)
 {
     var lastfetch  = new String();
