1badb914398975f00c17afbcf44f384efff2d00f
Ramiro Polla <ramiro.polla@gmail.com>
2011-12-07 23:37:07 -0200

    ui_saver: ui_logger->ui_saver

--- ui_saver.js
+++ ui_saver.js
@@ -2,19 +2,19 @@ function UI_saver(id, identificador)
 {
     var self = this;
 
-    var ui_logger = document.getElementById(id).parentNode;
-    ui_logger.style.fontFamily = "monospace";
-    ui_logger.style.fontSize   = "13px";
-    ui_logger.style.width = "300px";
-    ui_logger.appendChild(document.createTextNode("identificador: "));
+    var ui_saver = document.getElementById(id).parentNode;
+    ui_saver.style.fontFamily = "monospace";
+    ui_saver.style.fontSize   = "13px";
+    ui_saver.style.width = "300px";
+    ui_saver.appendChild(document.createTextNode("identificador: "));
     var input = document.createElement("input");
     input.style.fontFamily = "monospace";
     input.style.fontSize   = "11px";
     input.style.width = "90px"
     if (identificador)
         input.value = identificador;
-    ui_logger.appendChild(input);
-    ui_logger.appendChild(document.createTextNode(" "));
+    ui_saver.appendChild(input);
+    ui_saver.appendChild(document.createTextNode(" "));
     var button = document.createElement("span");
     button.style.MozUserSelect = "none";
     button.style.KhtmlUserSelect = "none";
@@ -25,8 +25,8 @@ function UI_saver(id, identificador)
     button.title = "salvar estado do programa";
     button.onselectstart = function () { return false; };
     button.onclick = function () { self.cb_salvar(input.value); return false; };
-    ui_logger.appendChild(button);
-    ui_logger.appendChild(document.createTextNode(" "));
+    ui_saver.appendChild(button);
+    ui_saver.appendChild(document.createTextNode(" "));
     var button = document.createElement("span");
     button.style.MozUserSelect = "none";
     button.style.KhtmlUserSelect = "none";
@@ -37,7 +37,7 @@ function UI_saver(id, identificador)
     button.title = "carregar estado do programa";
     button.onselectstart = function () { return false; };
     button.onclick = function () { self.cb_carregar(input.value); return false; };
-    ui_logger.appendChild(button);
+    ui_saver.appendChild(button);
 
     self.input = input;
 
