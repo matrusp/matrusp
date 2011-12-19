43f61503457d6b27f2d2b4404a81cb54f19d438f
Ramiro Polla <ramiro.polla@gmail.com>
2011-12-18 18:30:54 -0800

    fix scrollbar_width

--- main.js
+++ main.js
@@ -590,14 +590,36 @@ function Main(combo, ui_materias, ui_turmas, ui_logger, ui_combinacoes, ui_horar
     };
 }
 
+function getScrollBarWidth () {
+  var inner = document.createElement('p');
+  inner.style.width = "100%";
+  inner.style.height = "200px";
+
+  var outer = document.createElement('div');
+  outer.style.position = "absolute";
+  outer.style.top = "0px";
+  outer.style.left = "0px";
+  outer.style.visibility = "hidden";
+  outer.style.width = "200px";
+  outer.style.height = "150px";
+  outer.style.overflow = "hidden";
+  outer.appendChild (inner);
+
+  document.body.appendChild (outer);
+  var w1 = inner.offsetWidth;
+  outer.style.overflow = 'scroll';
+  var w2 = inner.offsetWidth;
+  if (w1 == w2) w2 = outer.clientWidth;
+
+  document.body.removeChild (outer);
+
+  return (w1 - w2);
+};
+
 ajuda_shown = false;
 mudancas = false;
 window.onload = function() {
-    var scrollbar_test = document.createElement("div");
-    scrollbar_test.style.overflow = "scroll";
-    document.body.appendChild(scrollbar_test);
-    document.scrollbar_width = scrollbar_test.offsetWidth - scrollbar_test.clientWidth;
-    document.body.removeChild(scrollbar_test);
+    document.scrollbar_width = getScrollBarWidth();
 
     var persistence = new Persistence();
     var identificador = persistence.read_id();
