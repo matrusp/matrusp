d6acf2a7e2e51fe87308e03306bd50925699c2f2
Ramiro Polla <ramiro.polla@gmail.com>
2012-02-07 14:57:05 -0200

    add simple compat for IE7 (still doesn't work)

---
+++ compat.js
@@ -0,0 +1,16 @@
+if (!('forEach' in Array.prototype)) {
+    Array.prototype.forEach= function(action, that /*opt*/) {
+        for (var i= 0, n= this.length; i<n; i++)
+            if (i in this)
+                action.call(that, this[i], i, this);
+    };
+}
+if (!('map' in Array.prototype)) {
+    Array.prototype.map= function(mapper, that /*opt*/) {
+        var other= new Array(this.length);
+        for (var i= 0, n= this.length; i<n; i++)
+            if (i in this)
+                other[i]= mapper.call(that, this[i], i, this);
+        return other;
+    };
+}
