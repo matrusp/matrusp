35c0acf63862ed14b6b15a9614132c1e34454718
Ramiro Polla <ramiro.polla@gmail.com>
2012-02-07 16:22:38 -0200

    compat: add Array.filter()

--- compat.js
+++ compat.js
@@ -14,3 +14,12 @@ if (!('map' in Array.prototype)) {
         return other;
     };
 }
+if (!('filter' in Array.prototype)) {
+    Array.prototype.filter= function(filter, that /*opt*/) {
+        var other= [], v;
+        for (var i=0, n= this.length; i<n; i++)
+            if (i in this && filter.call(that, v= this[i], i, this))
+                other.push(v);
+        return other;
+    };
+}
