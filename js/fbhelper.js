9b3ae266a1bb57b2ba3cb95923bf5bc573835b1f
Rafael <raverona@hotmail.com>
2016-10-27 16:10:20 -0200

    Update fbhelper.js to use unique identifier per session

--- js/fbhelper.js
+++ js/fbhelper.js
@@ -1,9 +1,14 @@
 document.getElementById('fbshare').addEventListener('click', generate_identifier);
 
 function generate_identifier() {
-  var identifier = (+new Date).toString(36);
-  ui.saveStateOnServer(identifier);
-  console.log(window.location.href + '#' + identifier);
+  var identifier;
+  if (sessionStorage.getItem('identifier') == null) {
+    identifier = (+new Date).toString(36);
+    sessionStorage.setItem('identifier', identifier);
+  } else {
+    identifier = sessionStorage.getItem('identifier');
+  }
+  this.saveStateOnServer(identifier);
   window.open("https://www.facebook.com/sharer/sharer.php?u="+encodeURIComponent(window.location.href + '#' + identifier), '',
     'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');
 }
