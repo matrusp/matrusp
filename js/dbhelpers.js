ea2c878f5fbb05fc346673b90b83b72c7fb60e45
Samuel Libardi Godoy <samlibardi@outlook.com>
2018-02-05 02:35:34 -0200

    Added web workers to update and search database Lecture and trigram info now stored in IndexedDB instead of memory. Update worker will fetch json and update accordingly. Search worker will read IndexedDB in separate thread and message search result.

---
+++ js/dbhelpers.js
@@ -0,0 +1,99 @@
+var stopwords_ptBR = [
+"DE",
+"DA",
+"DO",
+"DAS",
+"DOS",
+"A",
+"EM",
+"NO",
+"NA",
+"NOS",
+"NAS",
+"E",
+"O",
+"AO",
+"AS",
+"OS",
+"AOS",
+"PARA",
+"POR"
+];
+
+function changingSpecialCharacters(word) {
+  return word.toUpperCase()
+    .replace(/[ÀÁÂÃÄÅ]/g, "A")
+    .replace(/Ç/g, "C")
+    .replace(/[ÈÉÊË]/g, "E")
+    .replace(/[ÌÎÍÏ]/g, "I")
+    .replace(/Ð/g, "D")
+    .replace(/Ñ/g, "N")
+    .replace(/[ÒÓÔÕÖØ]/g, "O")
+    .replace(/[ÙÚÛÜ]/g, "U")
+    .replace(/Ý/g, "Y")
+    .replace(/ß/g, "SS");
+}
+
+function romanize(num) {
+  var lookup = {
+      M: 1000,
+      CM: 900,
+      D: 500,
+      CD: 400,
+      C: 100,
+      XC: 90,
+      L: 50,
+      XL: 40,
+      X: 10,
+      IX: 9,
+      V: 5,
+      IV: 4,
+      I: 1
+    },
+    roman = '';
+  for (var i in lookup) {
+    while (num >= lookup[i]) {
+      roman += i;
+      num -= lookup[i];
+    }
+  }
+  return roman;
+}
+
+function trigramsFromString(str,asAcronym) {
+  var trigrams = new Array();
+
+  str = str.replace(/(\b[IVXLCM]+)\s+(?=[IVXLCM])/g,"$1");
+
+  var words = str.split(" ");
+
+  if (words.length > 1 && !isNaN(words[words.length - 1]))
+    words[words.length - 1] = romanize(parseInt(words[words.length - 1]));
+
+  words = words.filter(function(word,i) { 
+      return i == 0 || stopwords_ptBR.indexOf(word) === -1;
+  }.bind(this));
+
+  for (var i = 0; i < words.length; i++) {
+    var word = words[i];
+
+    trigrams.push(word[0] + "#");
+    if (i === 0 && word.length > 2) trigrams.push(word[0] + word[1] + word[2] + "!");
+    trigrams.push(word + "$"); //exact word match
+
+    for (var j = 0; j < word.length; j++) {
+      if (j < word.length - 2)
+        trigrams.push(word[j] + word[j + 1] + word[j + 2]);
+      if (asAcronym && word.length < 5) { //small first words will be treated as acronyms e.g GA, SD
+        if (j > 0) trigrams.push(word[j - 1] + word[j] + "%");
+      }
+    }
+
+    if (i > 0)
+      trigrams.push(words[i - 1][0] + words[i][0] + "%"); //sequential first letters
+  }
+
+  trigrams.push(str + "&"); //exact match
+
+  return trigrams;
+}
---
+++ js/dbsearch.js
@@ -0,0 +1,57 @@
+self.importScripts("dbhelpers.js");
+
+self.onmessage = e => {
+  if(!e.data || e.data === '') self.close();
+
+  var dbrequest = self.indexedDB.open("MatruspDB");
+  
+  dbrequest.onsuccess = ev => {
+    self.idb = ev.target.result;
+    fetchLectureOnDB (e.data);
+  };
+
+  dbrequest.onerror = e => self.close(); //TODO: handle db opening error;
+};
+
+
+function fetchLectureOnDB (word) {
+  var result = [];
+  
+  //Search for exact code in the lectures database. In case of a match, send message with the found lecture and kill the worker.
+  if(word.length === 7) //Exact code match will only happen if search is 7 characters long
+  {
+    var request = self.idb.transaction("lectures").objectStore("lectures").get(word);
+    request.onsuccess = e => {
+      if(e.target.result) {
+        self.postMessage([e.target.result]); //Send an array with the found lecture because the reciever will be expecting an array;
+        self.close(); //End this worker's operation
+      }
+    }
+  }
+  
+  word = changingSpecialCharacters(word).trim();
+  var scores = new Object();
+  var transaction = self.idb.transaction(["trigrams","lectures"]);
+
+  trigramsFromString(word,true).forEach(trigram => {
+    transaction.objectStore("trigrams").get(trigram).onsuccess = e => {
+      for(var code in e.target.result)
+      {
+        if(!scores[code]) { 
+          scores[code] = 0;
+          transaction.objectStore("lectures").get(code).onsuccess = e => result.push(e.target.result);
+        }
+        scores[code] += e.target.result[code];
+      }
+    }
+  });
+
+  transaction.oncomplete = e => {
+    result.sort(function(first, second) {
+      return scores[second.code]/Math.log(3+second.name.length) - scores[first.code]/Math.log(3+first.name.length);
+    });
+
+    self.postMessage(result.slice(0,50));
+    self.close();
+  }
+}
