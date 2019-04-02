f6eb9107997b8abd23295cc93df5dc8b7863eef7
Samuel Libardi Godoy <samlibardi@outlook.com>
2019-04-02 16:57:28 -0300

    Contact naming consistency

--- js/contact.js
+++ js/contact.js
@@ -1,12 +1,12 @@
-document.getElementById("contato").addEventListener('click', e => {
+document.getElementById("contact").addEventListener('click', e => {
    var msg =
 `\n
-=======================================================================================================\n
+===========================================================\n
 Deixe aqui sua dúvida ou sugestão.\n\n
 Em caso de problemas, tente explicar da forma mais completa possível para que possamos identificar a causa. Tente acrescentar o que estava fazendo no momento, quais disciplinas estavam selecionadas e o passo a passo para causar o problema. Obrigado.\n
-=======================================================================================================\n\n
+===========================================================\n\n
 Informações úteis para nós: ${btoa(state.toJSON())}\n
-=======================================================================================================\n\n`;
+===========================================================\n\n`;
 
    location.href = "mailto:apoiobcc@linux.ime.usp.br?subject=D%C3%BAvida/Sugest%C3%A3o%20MatrUSP&body=" + encodeURI(msg); 
 
